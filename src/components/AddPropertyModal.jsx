074b98546bca2b5e48beaf8ffbf809d3ace19cca
harsh9291 <harshharsh815000@gmail.com>
2025-10-27 15:09:06 +0530

    updrage

--- src/components/AddPropertyModal.jsx
+++ src/components/AddPropertyModal.jsx
@@ -758,7 +758,7 @@ const handleChangeImage = (e) => {
                 <div>
                   <label className="form-label fw-bold">Area(sqft) *</label>
                   <input
-                    type="number"
+                    type="text"
                     className="w-full p-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
                     name="area"
                     data-nested="propertyDetails"
@@ -766,6 +766,7 @@ const handleChangeImage = (e) => {
                     onChange={handleChange}
                     placeholder="Enter area (e.g., 1200 sqft)"
                     required
+                    readOnly
                   />
                 </div>
 
